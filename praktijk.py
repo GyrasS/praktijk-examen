40ed357f35eb76f88fc7939b0fd486ee0192c88c
Ron <clutchfox2004@gmail.com>
2024-10-14 01:52:23 +0200

    praktijk toegevoegd

--- praktijk.py
+++ praktijk.py
@@ -5,15 +5,13 @@ werk = input(f"Voer in: medewerker, zelfstandige of ambtenaar: ") # invoer van w
 
 werkstatuut = {"medewerker" : [350, 20], "zelfstandige" : [300, 15], "ambtenaar" : [370, 25]} # dictionary, keys is de werkstatuut en values zijn lists met daar in de hoeveelheid euro
 
-def pensioen():
-    global all
-
+def pensioen(leeftijd, werk, werkstatuut):
     if leeftijd < 65:
         verschil = 65 - leeftijd # berekend het verschil tussen het pensioen leeftijd, en de ingevoerde leeftijd
-        print(f"Van werken word je gelukkig, je mag nog {verschil} jaar genieten van je baan") # uitvoer als leeftijd te laag is
+        return f"Van werken word je gelukkig, je mag nog {verschil} jaar genieten van je baan" # uitvoer als leeftijd te laag is
     else:
         if leeftijd < 70:
-            print(f"Je krijgt € {werkstatuut[werk][0]} per week.") # uitvoer als leeftijd 65 tot 70 is
+            return f"Je krijgt € {werkstatuut[werk][0]} per week." # uitvoer als leeftijd 65 tot 70 is
         else:
-            print(f"Je krijgt € {werkstatuut[werk][0] + werkstatuut[werk][1]} per week.") # uitvoer als leeftijd boven 70 is
-pensioen()
+            return f"Je krijgt € {werkstatuut[werk][0] + werkstatuut[werk][1]} per week." # uitvoer als leeftijd boven 70 is
+print(pensioen(leeftijd, werk, werkstatuut))
